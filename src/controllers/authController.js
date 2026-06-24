b7c30a5fd051e7aed9319c117129d54140c9bd84
Charandeep Singh <N01748813@humber.ca>
2026-06-23 20:01:51 -0400

    feat: add login endpoints with bcrypt password verification

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -33,4 +33,43 @@ const register = async (req, res ) =>{
     }
 };
 
-module.exports= {register};
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email || !password){
+        return res.status(400).json({error: 'All fields are required!'});
+    }
+
+    try{
+        const user = await pool.query(
+            'SELECT * FROM users WHERE email = $1', [email]
+        );
+
+        if(user.rows.length === 0){
+            return res.status(400).json({error: 'Invalid credentials!'});
+        }
+
+        const validPassword = await bcrypt.compare(
+            password,
+            user.rows[0].password_hash
+        );
+        if(!validPassword){
+            return res.status(400).json({error: 'Invalid credentials!'});
+        }
+
+        res.status(200).json({
+            message: 'Login successful!',
+            user:{
+                id: user.rows[0].id,
+                first_name: user.rows[0].first_name,
+                email: user.rows[0].email
+
+            }
+        });
+    }catch(error){
+        console.error(error)
+        res.status(500).json({error: 'Server error'})
+    }
+}
+
+module.exports= {register, login};
